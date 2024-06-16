5c6372311e3c78cbd510ac1e85797352d5279a5d
XATAB1CH <demidovanton34@gmail.com>
2024-06-16 23:58:17 +0300

    Первый коммит

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/XATAB1CH/achievement-holder
+
+go 1.22.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	username string
+	loggedIn bool
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// логика ввода пароля
+	logWord := "Авторизация успешна"
+	fmt.Fprint(w, logWord)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	data := User{
+		username: "admin",
+		loggedIn: false,
+	}
+
+	tmpl, _ := template.ParseFiles("templates/index.html")
+	tmpl.Execute(w, data)
+}
+
+func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/products/{id:[0-9]+}", loginHandler)
+
+	router.HandleFunc("/", indexHandler)
+	http.Handle("/", router)
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8080", nil)
+}
